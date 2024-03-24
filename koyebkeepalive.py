334a61e6baa028c13e63104153c52b36c6352f76
fxf981 <yeyunet@gmail.com>
2024-03-25 07:56:49 +0800

    123

--- koyebkeepalive.py
+++ koyebkeepalive.py
@@ -1,35 +1,19 @@
 import requests
 import sys
 import os
-
-# 定义一些变量
-desp = ""
+import time
 
 def log(info: str):
     print(info)
     global desp
     desp = desp + info + "\n"
 
-
 url = "https://555-vms.koyeb.app/"
 
-# 定义浏览器请求头
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
 }
 
-# 发送GET请求
-response = requests.get(url, headers=headers)
-
-# 检查响应状态码
-if response.status_code == 200:
-    log("请求成功！")
-    # log("响应内容：" + str(response.text))
-else:
-    log("请求失败，状态码：" + str(response.status_code))
-
-
-# 推送信息
 TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
 TG_USER_ID = os.environ.get('TG_USER_ID')
 
@@ -49,7 +33,15 @@ def telegram():
     except Exception as e:
         log(f"Telegram推送时出错: {str(e)}")
 
-# 消息推送
-if TG_BOT_TOKEN and TG_USER_ID and len(desp) > 0:
-    telegram()
-    sys.exit(0)
+desp = ""
+
+for _ in range(5):
+    response = requests.get(url, headers=headers)
+    log(f"状态码：{response.status_code}")
+
+    if TG_BOT_TOKEN and TG_USER_ID and len(desp) > 0:
+        telegram()
+    
+    time.sleep(5)
+
+sys.exit(0)
