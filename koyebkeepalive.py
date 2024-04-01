c8142113aa247b1a7f7c04add5af8e888c8e3858
fxf981 <yeyunet@gmail.com>
2024-04-01 16:50:49 +0800

    q

--- koyebkeepalive.py
+++ koyebkeepalive.py
@@ -8,7 +8,7 @@ def log(info: str):
     global desp
     desp = desp + info + "\n"
 
-url = "https://555-vms.koyeb.app/"
+url = "https://gentle-felidae-vz.koyeb.app"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
